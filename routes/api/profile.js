a11f04ec3de50b1d90a0c16ced8ad4041f59e493
anantm007 <anant.mathur007@gmail.com>
2019-07-01 17:07:36 +0530

    Complete Profile Section on the backend

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,8 @@
 //jshint esversion: 8
 
 const express = require("express");
+const request = require("request");
+const config = require("config");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 
@@ -306,5 +308,128 @@ router.delete("/experience/:exp_id", auth,
       res.status(500).send("Server Error");
     }
 });
+
+
+// @route   PUT api/profile/education
+// @desc    Add profile education
+// @access  Private
+
+router.put("/education", [auth, [
+  check("school", "school is required").not().isEmpty(),
+  check("degree", "degree is required").not().isEmpty(),
+  check("fieldofstudy", "field of study date is required").not().isEmpty(),
+  check("from", "from is required").not().isEmpty()
+] ],
+
+async (req, res) => {
+
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty())
+  return res.status(400).json({ errors: errors.array()});
+
+  // Destructuring
+  const {
+    school,
+    degree,
+    fieldofstudy,
+    from,
+    to,
+    current,
+    description
+  } = req.body;
+
+
+  // Create new experience
+  const newEdu = {
+    school,
+    degree,
+    fieldofstudy,
+    from,
+    to,
+    current,
+    description
+  };
+
+  // MongoDB dealing
+  try {
+    const profile = await Profile.findOne({user: req.user.id});
+
+    profile.education.unshift(newEdu); // push() appends but unshift() adds at the beginning
+
+
+  await profile.save();
+  res.json(profile);
+
+}
+  catch(err)
+  {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+
+});
+
+
+// @route  DELETE api/profile/education/:edu_id
+// @desc   Delete education
+//@access  Private
+
+router.delete("/education/:edu_id", auth,
+  async (req,res) => {
+    try
+    {
+      const profile = await Profile.findOne({user: req.user.id});
+
+      // Get remove index
+      const removeIndex = profile.education.map(item => item.id)
+      .indexOf(req.params.exp_id);
+
+      // Deleting or splicing or taking out from experience array
+      profile.education.splice(removeIndex, 1);
+
+      await profile.save();
+
+      res.json(profile);
+    }
+
+    catch(err)
+    {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+    }
+});
+
+
+// @route    GET api/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        'githubClientId'
+      )}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No Github profile found' });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 
 module.exports = router;
