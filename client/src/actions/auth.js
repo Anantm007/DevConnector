932c63733103ef1ff58ece579206632e4875a2b9
anantm007 <anant.mathur007@gmail.com>
2019-08-26 17:25:16 +0530

    auth reducer

---
+++ client/src/actions/auth.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import {setAlert} from './alert';
+import {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL
+} from './type';
+
+// Register user
+export const register = ({name, email, password}) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    } 
+
+    const body = JSON.stringify({name, email, password});
+
+    try {
+        const res = await axios.post('/api/users', body, config);
+
+        dispatch({
+            type: REGISTER_SUCCESS,
+            payload: res.data
+        });
+    } catch(err) {
+
+        const errors = err.response.errors;
+
+        if(errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch ({
+            type: REGISTER_FAIL
+        });
+    }
+
+};
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,10 +2,11 @@ import React, {Fragment, useState} from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {setAlert} from '../../actions/alert';
+import {register} from '../../actions/auth';
 import PropTypes from 'prop-types';
-import { pathToFileURL } from 'url';
 
-const Register = ({setAlert}) => {
+
+const Register = ({setAlert, register}) => {
 
     const [formData, setFormData] = useState({
         name: '',
@@ -23,7 +24,9 @@ const Register = ({setAlert}) => {
         if(password !== password2)
             setAlert('Passwords do not match', 'danger');
         else
-        console.log("SUCCESS");
+        {
+          register({name, email, password});
+        }
     }
 
     return ( 
@@ -63,7 +66,10 @@ const Register = ({setAlert}) => {
 
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
 };
 
-export default connect(null, {setAlert})(Register);
+export default connect(null, 
+  {setAlert, register})
+  (Register);
