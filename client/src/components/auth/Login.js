80225728bcbc38aa19d668d21e133bf1ded7b3c4
anantm007 <anant.mathur007@gmail.com>
2019-08-26 18:55:17 +0530

    finish login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
-import React, {Fragment, useState} from 'react'
-import {Link} from 'react-router-dom'
+import React, {Fragment, useState} from 'react';
+import {Link, Redirect} from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
-const Login = () => {
+const Login = ({login, isAuthenticated }) => {
 
     const [formData, setFormData] = useState({
         email: '',
@@ -14,7 +17,12 @@ const Login = () => {
     
     const onSubmit = e => {
         e.preventDefault();
-        console.log("SUCCESS");
+        login(email, password);
+    }
+
+  //  Redirect if logged in
+    if(isAuthenticated) {
+      return <Redirect to = "/dashboard" />
     }
 
     return ( <Fragment> 
@@ -45,4 +53,14 @@ const Login = () => {
     );
 };
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+  
+}
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { login })(Login);
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -2,7 +2,9 @@ import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
     USER_LOADED,
-    AUTH_ERROR
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
 } from '../actions/type';
 
 const initialState = {
@@ -25,6 +27,7 @@ export default function(state = initialState, action, ) {
                 user: payload
             }
         case REGISTER_SUCCESS:
+        case LOGIN_SUCCESS:
             localStorage.setItem('token', payload.token);
       return {
         ...state,
@@ -35,6 +38,7 @@ export default function(state = initialState, action, ) {
 
         case REGISTER_FAIL:
         case AUTH_ERROR:
+        case LOGIN_FAIL:
             localStorage.removeItem('token');
             return  {
                 ...state,
