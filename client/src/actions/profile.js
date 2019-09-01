b5043a21333c18e2b90b82814b2ca16f0a32679f
anantm007 <anant.mathur007@gmail.com>
2019-09-01 15:29:44 +0530

    create profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -19,3 +19,37 @@ export const getCurrentProfile = () => async dispatch => {
     });
   }
 };
+
+// Create or update profile
+export const createProfile = (FormData, history, edit = false) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const res = await axios.post('/api/profile', FormData, config);
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert(edit? 'Profile Updated': 'Profile Created', 'success'));
+
+    if(!edit)
+    history.push('/dashboard');
+
+  } catch (err) {
+    
+    const errors = err.response.errors;
+
+    if(errors) {
+        errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+} 
