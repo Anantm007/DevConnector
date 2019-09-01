0458d756e025f92d49fb5acd6921637a636514cb
anantm007 <anant.mathur007@gmail.com>
2019-09-01 18:55:46 +0530

    display education and experiences

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,6 +5,8 @@ import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile';
 import Spinner from '../layout/Spinner';
 import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 
 const Dashboard = ({ getCurrentProfile, auth: {user}, profile: { profile, loading} }) => {
   useEffect(() => {
@@ -19,6 +21,8 @@ const Dashboard = ({ getCurrentProfile, auth: {user}, profile: { profile, loadin
         {profile !== null ? 
         <Fragment>
           <DashboardActions/>
+          <Experience experience = {profile.experience}/>
+          <Education education = {profile.education}/>
         </Fragment> : 
         <Fragment>
             <p>You have not setup a profile yet, please add some information</p>
---
+++ client/src/components/dashboard/Education.js
@@ -0,0 +1,50 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+import moment from 'moment';
+import { connect } from 'react-redux';
+
+const Education = ({ education }) => {
+  const educations = education.map(edu => (
+    <tr key={edu._id}>
+      <td>{edu.school}</td>
+      <td className="hide-sm">{edu.degree}</td>
+      <td>
+        <Moment format="YYYY/MM/DD">{moment.utc(edu.from)}</Moment> -{' '}
+        {edu.to === null ? (
+          ' Now'
+        ) : (
+          <Moment format="YYYY/MM/DD">{moment.utc(edu.to)}</Moment>
+        )}
+      </td>
+      <td>
+        <button className="btn btn-danger">Delete</button>
+        
+      </td>
+    </tr>
+  ));
+
+  return (
+    <Fragment>
+      <h2 className="my-2">Education Credentials</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>School</th>
+            <th className="hide-sm">Degree</th>
+            <th className="hide-sm">Years</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>{educations}</tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+Education.propTypes = {
+  education: PropTypes.array.isRequired,
+  
+};
+
+export default (Education);
