249b063f218fef49f6e3cace09a7c75d067bc8de
Anant Mathur <anant.mathur007@gmail.com>
2019-09-07 13:31:38 +0530

    Fix go back button

--- client/src/components/profile-forms/AddEducation.js
+++ client/src/components/profile-forms/AddEducation.js
@@ -118,7 +118,7 @@ const AddEducation = ({ addEducation, history }) => {
           />
         </div>
         <input type='submit' class='btn btn-primary my-1' />
-        <a class='btn btn-light my-1' href='dashboard.html'>
+        <a class='btn btn-light my-1' href='/dashboard'>
           Go Back
         </a>
       </form>
